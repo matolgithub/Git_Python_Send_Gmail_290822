7c584e08075604d39b6a6df082c03ceda8ff4e37
matolgithub <89175955650@mail.ru>
2022-08-29 21:57:12 +0300

    Second commit.

---
+++ main.py
@@ -0,0 +1,17 @@
+import smtplib
+
+
+class Mail:
+    def __init__(self, mail='89175955650@mail.ru'):
+        self.mail = mail
+
+    def send_email(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    mail_object = Mail()
+    mail_object.main()
