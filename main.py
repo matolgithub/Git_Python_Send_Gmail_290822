8a21f4475f6b34b48b5359f75d710e244e2dace4
matolgithub <89175955650@mail.ru>
2022-08-29 23:16:13 +0300

    Commit: gmail.com - success!

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import smtplib
 
-
+# GMAIL.COM
 class Mail:
-    def __init__(self, mail='89175955650@mail.ru'):
+    def __init__(self, mail='matolpydev@gmail.com'):
         self.mail = mail
 
-    def send_email(self):
-        pass
+    def send_email(self, message="Hi! It's message from the python code!"):
+        sender = "matolpydev@gmail.com"
+        password = "csszpkcslndaiita"
 
-    def main(self):
-        pass
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+
+        try:
+            server.login(user=sender, password=password)
+            server.sendmail(from_addr=sender, to_addrs=self.mail, msg=message)
+            sent_result = "The message was sent successfully!"
+        except Exception as _ex:
+            sent_result = f"{_ex}\nCheck your login or password, please!"
+        print(sent_result)
+
+        return sent_result
 
 
 if __name__ == '__main__':
     mail_object = Mail()
-    mail_object.main()
+    mail_object.send_email()
